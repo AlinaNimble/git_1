e2841103b8f494a2a6315e23599c8a71f8e2d3b6
Alina <yurkova-alina@mail.ru>
2022-10-10 22:34:37 +0300

    Tasks 10,13,15

---
+++ Task15/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+string CheckВayOff(int NumDay)
+{
+    string YesNo;
+    if (NumDay > 5)
+    {
+        YesNo = "да";    
+    }
+    else
+    {
+        YesNo = "нет";    
+    }
+
+    return YesNo;
+}
+
+Console.Write("Введите номер дня недели: ");
+string NumDayStr;
+NumDayStr = Console.ReadLine(); // с консоли получаем тип string всегда
+
+int NumDay;
+int.TryParse(NumDayStr, out NumDay); //преобразуем String в Int
+
+string otvet = CheckВayOff(NumDay); //вызываем Метод CheckВayOff, который принимает аргумент NumDay и возвращает ответ
+
+Console.WriteLine(otvet);
