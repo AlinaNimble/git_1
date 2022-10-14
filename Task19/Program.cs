9b4bd3cb9b1b2d17a9da5b7ff747f23bfd48960f
Alina <yurkova-alina@mail.ru>
2022-10-14 06:50:35 +0300

    Tasks 19, 21, 23

---
+++ Task19/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+string CheckPalindrom(int Chislo)
+{
+    string chisloStr = Chislo.ToString();
+
+    string YesNo = "да";
+
+    for(int i=0; i < (chisloStr.Length % 2); i++)
+    {
+            if (chisloStr[i]!=chisloStr[chisloStr.Length - i - 1]!)
+            {
+                YesNo = "нет";
+                break;
+            }
+    }
+    return YesNo;
+}
+
+Console.Write("Введите пятизначное число: ");
+string ChisloStr;
+ChisloStr = Console.ReadLine(); // с консоли получаем тип string всегда
+
+int ChisloInt;
+int.TryParse(ChisloStr, out ChisloInt); //преобразуем String в Int
+
+string otvet = CheckPalindrom(ChisloInt); //вызываем Метод CheckPalindrom, который принимает аргумент ChisloInt и возвращает ответ
+
+Console.WriteLine(otvet);
