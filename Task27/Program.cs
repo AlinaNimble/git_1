7395b6c97073fd84c5b3cdee50f15abbf1438573
Alina <yurkova-alina@mail.ru>
2022-11-01 00:25:41 +0300

    Tasks 25,27,29

---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int A = 452;
+
+string B = $"{A}";                  // int -> String     (можно еще A.ToString())
+                                    // переводим для того, что string - это всего лишь МАССИВ из символов (char)!
+
+int result=0;
+
+for (int i=0; i<B.Length; i++)      // B - строка, т.е. массив символов 
+{
+    string C = B[i].ToString();     // Char -> String
+
+    result = result + int.Parse(C); // String -> int
+    
+}
+
+Console.WriteLine(result);
